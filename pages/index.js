3ed0bf28ac03511d9de5ecea49be71bce3bd0f26
huddhamoon <hy51hy@126.com>
2023-05-05 15:47:44 +0800

    dev

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ allPostsData }) {
 
       {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Blog - 中文显示</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
