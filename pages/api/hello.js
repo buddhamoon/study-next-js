7d7898f080cebe0d119cf504bad01c55fd2e750b
huddhamoon <hy51hy@126.com>
2023-05-05 14:28:25 +0800

    dev

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,6 +4,7 @@ export default function handler(req, res) {
       // res.status(200).send({ text: 'Hello' });
       res.status(200).json({ text: 'Hello' });
     } else {
+      // test
       res.status(404).send({ 'errorMessage': '404' });
     }
   }
